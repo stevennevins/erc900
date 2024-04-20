019d7cbde84e53b2a7ba4984158ef22eb3a731c4
steven <steven.nevins.js@gmail.com>
2024-04-20 18:55:24 -0400

    refactor: clean up

--- src/ERC900.sol
+++ src/ERC900.sol
@@ -1,78 +1,85 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
-import {Ownable} from "../lib/openzeppelin-contracts/contracts/access/Ownable.sol";
-import {SafeERC20} from "../lib/openzeppelin-contracts/contracts/token/ERC20/utils/SafeERC20.sol";
-import {IERC20} from "../lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
-import {EnumerableSet} from "../lib/openzeppelin-contracts/contracts/utils/structs/EnumerableSet.sol";
-import {IERC900} from "./IERC900.sol";
+import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "./IERC900.sol";
 
-contract ERC900 is IERC900 {
+contract ERC900 is IERC900{
     using SafeERC20 for IERC20;
     using EnumerableSet for EnumerableSet.AddressSet;
 
-    address public token;
+    address private _token;
 
-    // Mapping to keep track of each staker's total stake.
-    mapping(address => uint256) internal _stakes;
-    // Mapping to keep track of the total staked amount.
-    uint256 internal _totalStaked;
+    mapping(address => uint256) private _stakes;
+    EnumerableSet.AddressSet private _stakers;
 
-    EnumerableSet.AddressSet internal stakers;
+    uint256 private _totalStaked;
 
-    constructor(address _token) {
-        token = _token;
+    constructor(address token_) {
+        _token = token_;
     }
 
-    function stake(uint256 amount, bytes calldata data) external {
-        stakeFor(msg.sender, amount, data);
+    function stake(uint256 amount, bytes calldata data) external override {
+        _stakeFor(msg.sender, msg.sender, amount, data);
     }
 
-    function stakeFor(address beneficiary, uint256 amount, bytes calldata data) public {
-        require(amount > 0, "Stake amount must be positive");
-        require(beneficiary != address(0), "Cannot stake for zero address");
-
-        _stakes[beneficiary] = _stakes[beneficiary] + amount;
-        _totalStaked = _totalStaked + amount;
-        if (_stakes[beneficiary]==amount) {
-            stakers.add(beneficiary);
-        }
-
-        emit Staked(beneficiary, amount, _stakes[beneficiary], data);
-
-        IERC20(token).safeTransferFrom(msg.sender, address(this), amount);
+    function stakeFor(address user, uint256 amount, bytes calldata data) external override {
+        _stakeFor(msg.sender, user, amount, data);
     }
 
-    function unstake(uint256 amount, bytes calldata data) external {
-        require(amount > 0, "Unstake amount must be positive");
-        require(_stakes[msg.sender] >= amount, "Insufficient stake");
-
-
-        _stakes[msg.sender] = _stakes[msg.sender] - amount;
-        _totalStaked = _totalStaked - amount;
-        if (_stakes[msg.sender] == 0) {
-            stakers.remove(msg.sender);
-        }
-
-        emit Unstaked(msg.sender, amount, _stakes[msg.sender], data);
-
-        IERC20(token).safeTransfer(msg.sender, amount);
+    function unstake(uint256 amount, bytes calldata data) external override {
+        _unstake(msg.sender, amount, data);
     }
 
-    function totalStakedFor(address addr) external view returns (uint256) {
-        return _stakes[addr];
+    function totalStakedFor(address user) external view override returns (uint256) {
+        return _stakes[user];
     }
 
-    function totalStaked() external view returns (uint256) {
+    function totalStaked() external view override returns (uint256) {
         return _totalStaked;
+    }
+
+    function isStaker(address user) external view returns (bool) {
+        return _stakers.contains(user);
+    }
+
+    function token() external view returns (address){
+        return _token;
     }
 
     function supportsHistory() external pure override returns (bool) {
         return true;
     }
 
-    function isStaker(address staker) external view returns (bool){
-        return stakers.contains(staker);
+    function _stakeFor(address payer, address user, uint256 amount, bytes memory data) internal {
+        require(amount > 0, "Stake amount must be positive");
+        require(user != address(0), "Cannot stake for zero address");
+
+        _totalStaked += amount;
+        _stakes[user] += amount;
+        _stakers.add(user);
+
+        emit Staked(user, amount, _stakes[user], data);
+
+        IERC20(_token).safeTransferFrom(payer, address(this), amount);
+    }
+
+    function _unstake(address user, uint256 amount, bytes memory data) internal {
+        require(amount > 0, "Unstake amount must be positive");
+        require(_stakes[user] >= amount, "Insufficient stake");
+
+        _stakes[user] -= amount;
+        _totalStaked -= amount;
+
+        if (_stakes[user] == 0) {
+            _stakers.remove(user);
+        }
+
+        emit Unstaked(user, amount, _stakes[user], data);
+
+        IERC20(_token).safeTransfer(user, amount);
     }
 }
 
